b7ffd993ed8b7cfd3c20e4e35188d443b89175dd
Hing Zi Feng <zfhing2107@gmail.com>
2026-04-06 14:38:43 +0800

    preregisteredlist model

--- campushub/user/models.py
+++ campushub/user/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from django.db import models 
+from django.conf import settings
 
-# Create your models here.
+class PreRegisteredAttendee(models.Model):
+    event = models.ForeignKey('feed.Event', on_delete=models.CASCADE, related_name='pre_registered')
+    
+    email = models.EmailField(max_length=255)
+    name = models.CharField(max_length=255, null=True, blank=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('event', 'email')
+
+    def __str__(self):
+        return f"{self.name or self.email} - {self.event.title}"
