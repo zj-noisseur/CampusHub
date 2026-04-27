e2ae3ae050c3937e3de2a9142c55b9623a04785b
Bruce Yong <bruceyongzijing@gmail.com>
2026-04-27 09:13:02 +0800

    Changed the route names to remove confusion with numerous dashborad endpoints

--- campushub/core/urls.py
+++ campushub/core/urls.py
@@ -9,10 +9,10 @@ from core.views.feed import feed
 app_name = 'core'
 urlpatterns = [
     path('directory/', directory, name='directory'),
-    path('dashboard/', dashboard_home, name='dashboard_home'),
-    path('dashboard/queue/', dashboard_task_queue, name='dashboard_task_queue'),
-    path('dashboard/task-status/', dashboard_task_status, name='dashboard_task_status'),
-    path('dashboard/action/', dashboard_action, name='dashboard_action'),
+    path('admin-site/dashboard/', dashboard_home, name='admin_dashboard'),
+    path('admin-site/dashboard/queue/', dashboard_task_queue, name='admin_task_queue'),
+    path('admin-site/dashboard/task-status/', dashboard_task_status, name='admin_task_status'),
+    path('admin-site/dashboard/action/', dashboard_action, name='admin_action'),
     path('state/<int:state_id>/universities/', universities, name='universities'),
     path('state/<int:state_id>/universities/<int:university_id>/clubs/', clubs, name='clubs'),
     path('feed/', feed, name='feed'),
