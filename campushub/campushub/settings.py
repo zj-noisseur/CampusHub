76479332b7db2a73feb36bc2a95c36aaa9020de1
Bruce Yong <bruceyongzijing@gmail.com>
2026-06-29 19:50:38 +0800

    Turned off debug mode

--- campushub/campushub/settings.py
+++ campushub/campushub/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = f'{os.environ.get("DJANGO_KEY")}'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
